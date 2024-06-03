449f0e74cb7768a8159c2265aa276f7a2ffd31aa
DenisBayandin <deniska.bayandin@yandex.ru>
2024-06-04 00:07:08 +0300

    Change constants and code for get all tikets abbreviation in bot

--- bot.py
+++ bot.py
@@ -5,43 +5,43 @@ from models import *
 from config import bot_token
 from tinkoff_data import get_prediction
 
-bot = telebot.TeleBot(bot_token)
-count_predictions = 0
-ticket_name = ""
+BOT = telebot.TeleBot(bot_token)
+COUNT_PREDICTIONS = 0
+TICKET_NAME = ""
 
 
-@bot.message_handler(content_types=['text'])
+@BOT.message_handler(content_types=['text'])
 def get_text_message(message):
     if message.text == "/start":
-        bot.send_message(message.from_user.id, "Напиши аббревиатуру тикета")
-        bot.register_next_step_handler(message, get_ticket_name)
+        BOT.send_message(message.from_user.id, "Напиши аббревиатуру тикета")
+        BOT.register_next_step_handler(message, get_ticket_name)
     else:
-        bot.send_message(message.from_user.id, "Введи что-нибудь другое! Для начала введите '/start'")
+        BOT.send_message(message.from_user.id, "Введи что-нибудь другое! Для начала введите '/start'")
 
 
 def get_ticket_name(message):
-    global ticket_name
-    all_tikets_abbreviation = ", ".join([", ".join([file.split("_")[0] for file in os.listdir("models")])])
+    global TICKET_NAME
+    all_tikets_abbreviation = ", ".join([file.split("_")[0] for file in os.listdir("models")])
     if message.text in all_tikets_abbreviation:
-        ticket_name = message.text
-        bot.send_message(message.from_user.id, "На какое количество дней ты хочешь получить прогноз?")
-        bot.register_next_step_handler(message, get_count_predictons)
+        TICKET_NAME = message.text
+        BOT.send_message(message.from_user.id, "На какое количество дней ты хочешь получить прогноз?")
+        BOT.register_next_step_handler(message, get_count_predictons)
     else:
-        bot.send_message(message.from_user.id, f"Пока мы не можем дать прогноз на тикет с аббревиатурой {message.text}, выбери "
+        BOT.send_message(message.from_user.id, f"Пока мы не можем дать прогноз на тикет с аббревиатурой {message.text}, выбери "
                                                f"что-то из этого: {all_tikets_abbreviation}")
-        bot.register_next_step_handler(message, get_text_message)
+        BOT.register_next_step_handler(message, get_text_message)
 
 
 def get_count_predictons(message):
-    global count_predictions, ticket_name
-    count_predictions = int(message.text)
-    bot.send_message(message.from_user.id, "Ожидайте... \u2764\u2764\u2764\u2764\u2764")
-    bot.send_photo(message.from_user.id, "https://gas-kvas.com/grafic/uploads/posts/2023-09/1695826322_gas-kvas-com-p-kartinki-s-kotikami-27.jpg")
-    result = get_prediction(ticket_name, count_predictions)
-    bot.send_message(message.from_user.id,
+    global COUNT_PREDICTIONS, TICKET_NAME
+    COUNT_PREDICTIONS = int(message.text)
+    BOT.send_message(message.from_user.id, "Ожидайте... \u2764\u2764\u2764\u2764\u2764")
+    BOT.send_photo(message.from_user.id, "https://gas-kvas.com/grafic/uploads/posts/2023-09/1695826322_gas-kvas-com-p-kartinki-s-kotikami-27.jpg")
+    result = get_prediction(TICKET_NAME, COUNT_PREDICTIONS)
+    BOT.send_message(message.from_user.id,
                      round(result, 2) if isinstance(result, float) or isinstance(result, int) else
                      "\n".join([f"{index + 1} день: {value_result:.2f} \u20BD" for index,
                      value_result in enumerate(result)]))
 
 
-bot.polling(none_stop=True, interval=0)
+BOT.polling(none_stop=True, interval=0)
